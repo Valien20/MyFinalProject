9fa3187ba327c436ac346b2055f3adbd32d68be3
Valien20 <sena.senturk.19@gmail.com>
2021-01-31 00:45:22 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{   //ıentity implement eden class 1 database tablosudur
+    public interface IEntity
+    {
+     
+    }
+}
